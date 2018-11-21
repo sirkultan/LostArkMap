37339dd5e59d78bae6719adbf9eabb29ca3b0a2d
Patrick Gustaevel <craieldev@gmail.com>
2018-11-20 21:56:21 -0500

    Lutheran East Map

---
+++ src/data/area_lutheran_east.js
@@ -0,0 +1,113 @@
+(function(){
+
+    let area = LAM.createArea("Lutheran East", {
+        kr: "루테란 동부",
+        path: "maps/areas/LutheranEast",
+        zoomLevel: 5
+    });
+
+    area.registerMap("Castle of Lutheran", {
+        kr: "루테란 성",
+        img: "CastleOfLutheran.png",
+        type: MapTypeEnum.City,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Diorika Plains", {
+        kr: "디오리카 평원",
+        img: "DiorikaPlains.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Solar Halo Hill", {
+        kr: "해무리 언덕",
+        img: "Solar Halo Hill.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Nursery Rhizome", {
+        kr: "배꽃나무 자생지",
+        img: "NurseryRhizome.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Black Rose Chapel", {
+        kr: "흑장미 교회당",
+        img: "BlackRoseChapel.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Ria Monogamies", {
+        kr: "라이아 단구",
+        img: "RiaMonogamies.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Borea Manor", {
+        kr: "보레아 영지",
+        img: "BoreaManor.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Mane Waves Harbor", {
+        kr: "갈기파도 항구",
+        img: "ManeWavesHarbor.png",
+        type: MapTypeEnum.City,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Crocnice Beach", {
+        kr: "크로커니스 해변",
+        img: "CrocniceBeach.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Crying Storm", {
+        kr: "크라잉스톰",
+        img: "CryingStorm.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("교회당 지하", {
+        kr: "흑장미 교회당",
+        img: "교회당 지하.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("King's Tomb", {
+        kr: "왕의무덤",
+        img: ".png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Lastra Forest", {
+        kr: "라스트라 숲",
+        img: "LastraForest.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+})();
