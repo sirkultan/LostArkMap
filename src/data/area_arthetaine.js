91a61be3aa7ec2fcdd3ebfd099a4c08670878335
Patrick Gustaevel <craieldev@gmail.com>
2018-11-20 21:00:43 -0500

    Arthetaine map

---
+++ src/data/area_arthetaine.js
@@ -0,0 +1,97 @@
+(function(){
+
+    let area = LAM.createArea("Arthetaine", {
+        kr: "아르데타인",
+        path: "maps/areas/Arthetaine",
+        zoomLevel: 4
+    });
+
+    area.registerMap("Cracked Land", {
+        kr: "갈라진 땅",
+        img: "CrackedLand.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Nebelhorn Lab", {
+        kr: "네벨호른 연구소",
+        img: "NebelhornLab.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Totrerich", {
+        kr: "토트리치",
+        img: "Totrerich.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Riza Waterfall", {
+        kr: "리제 폭포",
+        img: "RizaWaterfall.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Wind Hill", {
+        kr: "바람결 구릉지",
+        img: "WindHill.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Red Sand Desert", {
+        kr: "붉은 모래 사막",
+        img: "RedSandDesert.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Barren Passage", {
+        kr: "메마른 통로",
+        img: "BarrenPassage.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Stern", {
+        kr: "슈테른",
+        img: "Stern.png",
+        type: MapTypeEnum.City,
+        markers: [
+        ]
+    });
+
+    area.registerMap("크라테르의 심장", {
+        kr: "크라테르의 심장",
+        img: "크라테르의 심장.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("아이히만의 연구기지", {
+        kr: "아이히만의 연구기지",
+        img: ".png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Nest Of Battle Ants", {
+        kr: "전투 개미 둥지",
+        img: "NestOfBattleAnts.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+})();
