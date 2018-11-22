fa2a61d575f1158237c7de44eeac53271d6b1a3b
craiel <craieldev@gmail.com>
2018-11-22 09:26:56 -0500

    fixed bug in url parsing

--- src/lam.js
+++ src/lam.js
@@ -41,9 +41,10 @@ let LAM = (function(){
 
             $.urlParam = function(name){
                 var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-                if (results==null) {
-                    return null;
+                if (results === null) {
+                    return undefined;
                 }
+
                 return decodeURI(results[1]) || 0;
             };
 
