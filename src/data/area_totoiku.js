e7e8f04ea1bff54d4354e3ab1b61ed80dcdf418e
Patrick Gustaevel <craieldev@gmail.com>
2018-11-21 22:03:20 -0500

    Totoiku Map

---
+++ src/data/area_totoiku.js
@@ -0,0 +1,65 @@
+(function(){
+
+    let area = LAM.createArea("Totoiku", {
+        kr: "토토이크",
+        path: "maps/areas/Totoiku",
+        zoomLevel: 4
+    });
+
+    area.registerMap("모코코 마을", {
+        kr: "모코코 마을",
+        img: "모코코 마을.png",
+        type: MapTypeEnum.City,
+        markers: [
+        ]
+    });
+
+    area.registerMap("달콤한 숲", {
+        kr: "달콤한 숲",
+        img: "달콤한 숲.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("바다향기 숲", {
+        kr: "바다향기 숲",
+        img: "바다향기 숲.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("달콤한 숲", {
+        kr: "달콤한 숲",
+        img: "달콤한 숲.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("침묵하는 거인의 숲", {
+        kr: "침묵하는 거인의 숲",
+        img: "침묵하는 거인의 숲.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("해적 소굴", {
+        kr: "해적 소굴",
+        img: "해적 소굴.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("토토이크의 몸", {
+        kr: "토토이크의 몸",
+        img: "토토이크의 몸.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+})();
