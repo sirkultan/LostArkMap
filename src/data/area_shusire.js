dc4be23e77069bc0124f219350243d191dbf0b63
Patrick Gustaevel <craieldev@gmail.com>
2018-11-21 21:46:42 -0500

    Shusire Map

---
+++ src/data/area_shusire.js
@@ -0,0 +1,89 @@
+(function(){
+
+    let area = LAM.createArea("Shusire", {
+        kr: "슈샤이어",
+        path: "maps/areas/Shusire",
+        zoomLevel: 4
+    });
+
+    area.registerMap("Regens Village", {
+        kr: "리겐스 마을",
+        img: "RegensVillage.png",
+        type: MapTypeEnum.City,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Lake of Time", {
+        kr: "시간의 호수",
+        img: "LakeOfTime.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Frost Prison Plateau", {
+        kr: "서리 감옥 고원",
+        img: "FrostPrisonPlateau.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Frozen Sea", {
+        kr: "얼어붙은 바다",
+        img: "FrozenSea.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Ice Butterfly Cliff", {
+        kr: "얼음나비 절벽",
+        img: "IceButterflyCliff.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Blade Wind Hill", {
+        kr: "칼날 바람 언덕",
+        img: "BladeWindHill.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("서클사이드", {
+        kr: "서클사이드",
+        img: "서클사이드.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("거울의 미궁", {
+        kr: "거울의 미궁",
+        img: "거울의 미궁.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("바에단의 은신처", {
+        kr: "바에단의 은신처",
+        img: "바에단의 은신처.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("그림자 미궁", {
+        kr: "그림자 미궁",
+        img: "그림자 미궁.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+})();
