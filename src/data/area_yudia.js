3949c4594613a6de1b545b2f9a05405eabdac20c
Patrick Gustaevel <craieldev@gmail.com>
2018-11-21 20:58:42 -0500

    Yudia Map

---
+++ src/data/area_yudia.js
@@ -0,0 +1,41 @@
+(function(){
+
+    let area = LAM.createArea("Yudia", {
+        kr: "유디아",
+        path: "maps/areas/Yudia",
+        zoomLevel: 4
+    });
+
+    area.registerMap("Saland Hills", {
+        kr: "살란드 구릉지",
+        img: "SalandHills.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Ozhorn Hills", {
+        kr: "오즈혼 구릉지",
+        img: "OzhornHills.png",
+        type: MapTypeEnum.Continent,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Sapira Cave", {
+        kr: "사피라 동굴",
+        img: "SapiraCave.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+    area.registerMap("Morai Ruins", {
+        kr: "모라이 유적",
+        img: "MoraiRuins.png",
+        type: MapTypeEnum.Dungeon,
+        markers: [
+        ]
+    });
+
+})();
