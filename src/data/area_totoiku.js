95a47477fabdb280616994b2afaf909ad82ad0e2
Patrick Gustaevel <craieldev@gmail.com>
2018-11-24 11:00:37 -0500

    updated totoiku map

--- src/data/area_totoiku.js
+++ src/data/area_totoiku.js
@@ -6,49 +6,49 @@
         zoomLevel: 4
     });
 
-    area.registerMap("모코코 마을", {
+    area.registerMap("Mokoko Village", {
         kr: "모코코 마을",
         type: MapTypeEnum.City,
         markers: [
         ]
     });
 
-    area.registerMap("달콤한 숲", {
-        kr: "달콤한 숲",
+    area.registerMap("Rocky Forest", {
+        kr: "성큼바위 숲",
         type: MapTypeEnum.Continent,
         markers: [
         ]
     });
 
-    area.registerMap("바다향기 숲", {
+    area.registerMap("Ocean Scent Forest", {
         kr: "바다향기 숲",
         type: MapTypeEnum.Continent,
         markers: [
         ]
     });
 
-    area.registerMap("달콤한 숲", {
+    area.registerMap("Sweet Forest", {
         kr: "달콤한 숲",
         type: MapTypeEnum.Continent,
         markers: [
         ]
     });
 
-    area.registerMap("침묵하는 거인의 숲", {
+    area.registerMap("Silent Giants Forest", {
         kr: "침묵하는 거인의 숲",
         type: MapTypeEnum.Continent,
         markers: [
         ]
     });
 
-    area.registerMap("해적 소굴", {
+    area.registerMap("Pirate Lair", {
         kr: "해적 소굴",
         type: MapTypeEnum.Dungeon,
         markers: [
         ]
     });
 
-    area.registerMap("토토이크의 몸", {
+    area.registerMap("Totoiku's Body", {
         kr: "토토이크의 몸",
         type: MapTypeEnum.Dungeon,
         markers: [
