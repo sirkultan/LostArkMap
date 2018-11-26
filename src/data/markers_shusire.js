eeb2adca8791994284784f20f3000646c9014bdf
Patrick Gustaevel <craieldev@gmail.com>
2018-11-26 01:48:31 -0500

    small treasuremap change

--- src/data/markers_shusire.js
+++ src/data/markers_shusire.js
@@ -1,5 +1,13 @@
 (function () {
 
-    LAM.areaMarkerData['Shusire'] = [];
+    LAM.areaMarkerData['Shusire'] = [
+        {
+            "x": -252.98,
+            "y": 340.87,
+            "type": MarkerTypeEnum.TreasureMap,
+            "id": 0,
+            "hintImage": "shusire/TreasureMap1.jpg"
+        }
+    ];
 
 }());
