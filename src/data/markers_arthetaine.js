6b1fe6231be611182b262f038e41e648bbeb1eae
craiel <craieldev@gmail.com>
2018-11-26 10:10:39 -0500

    Markers for Stern and Cracked Land (Arthetaine)

--- src/data/markers_arthetaine.js
+++ src/data/markers_arthetaine.js
@@ -97,6 +97,377 @@
             "type": MarkerTypeEnum.Mokoko,
             "id": 12,
             "hintImage": "arthetaine/BarrenMokokoHint5.jpg"
+        },
+        {
+            "x": -334.62,
+            "y": 236.26,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 13
+        },
+        {
+            "x": -284.37,
+            "y": 239.26,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 14,
+            "title": "To Riza Waterfall"
+        },
+        {
+            "x": -284.37,
+            "y": 185.02,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 15,
+            "title": "To Cracked Land",
+            "teleportTo": [
+                -245,
+                201
+            ]
+        },
+        {
+            "x": -334.37,
+            "y": 191.77,
+            "type": MarkerTypeEnum.Vista,
+            "id": 16,
+            "popupText": "대도시 전경",
+            "hintImage": "arthetaine/SternVistaHint1.jpg"
+        },
+        {
+            "x": -311.81,
+            "y": 183.89,
+            "type": MarkerTypeEnum.HiddenQuest,
+            "id": 17,
+            "hintImage": "arthetaine/SternHiddenQuestHint1.jpg"
+        },
+        {
+            "x": -372.44,
+            "y": 154.52,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 18,
+            "popupText": "고뇌의 해답#4",
+            "hintImage": "arthetaine/SternHiddenStoryHint1.jpg"
+        },
+        {
+            "x": -314.81,
+            "y": 176.01,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 19,
+            "popupText": "고뇌의 해답#1",
+            "hintImage": "arthetaine/SternHiddenStoryHint2.jpg"
+        },
+        {
+            "x": -333.69,
+            "y": 225.38,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 20,
+            "popupText": "고뇌의 해답#2",
+            "hintImage": "arthetaine/SternHiddenStoryHint3.jpg"
+        },
+        {
+            "x": -305.22,
+            "y": 249.25,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 21,
+            "popupText": "고뇌의 해답#3",
+            "hintImage": "arthetaine/SternHiddenStoryHint4.jpg"
+        },
+        {
+            "x": -304.72,
+            "y": 250.25,
+            "type": MarkerTypeEnum.SecretPassage,
+            "id": 22,
+            "hintText": "별 거 없다. 심심하면 가보시라",
+            "hintImage": "arthetaine/SternSecretHint1.jpg"
+        },
+        {
+            "x": -296.41,
+            "y": 244.38,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 23,
+            "popupText": "최고의 진화체 #2",
+            "hintImage": "arthetaine/SternHiddenStoryHint5.jpg"
+        },
+        {
+            "x": -292.47,
+            "y": 245.38,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 24,
+            "popupText": "최고의 진화체 #3",
+            "hintImage": "arthetaine/SternHiddenStoryHint6.jpg"
+        },
+        {
+            "x": -291.66,
+            "y": 238.63,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 25,
+            "popupText": "최고의 진화체 #1",
+            "hintImage": "arthetaine/SternHiddenStoryHint7.jpg"
+        },
+        {
+            "x": -348.56,
+            "y": 139.27,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 26,
+            "hintImage": "arthetaine/SternMokokoHint1.jpg"
+        },
+        {
+            "x": -308.19,
+            "y": 168.27,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 27,
+            "hintImage": "arthetaine/SternMokokoHint2.jpg"
+        },
+        {
+            "x": -313.09,
+            "y": 185.01,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 28,
+            "hintImage": "arthetaine/SternMokokoHint3.jpg"
+        },
+        {
+            "x": -331.56,
+            "y": 203.76,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 29,
+            "hintImage": "arthetaine/SternMokokoHint4.jpg"
+        },
+        {
+            "x": -282.19,
+            "y": 139.27,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 30,
+            "hintImage": "arthetaine/SternMokokoHint5.jpg"
+        },
+        {
+            "x": -324.44,
+            "y": 220.63,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 31,
+            "hintImage": "arthetaine/SternMokokoHint6.jpg"
+        },
+        {
+            "x": -313.94,
+            "y": 249.38,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 32,
+            "hintImage": "arthetaine/SternMokokoHint7.jpg"
+        },
+        {
+            "x": -284.56,
+            "y": 196.01,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 33,
+            "hintImage": "arthetaine/SternMokokoHint8.jpg"
+        },
+        {
+            "x": -261.47,
+            "y": 210.07,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 34,
+            "hintImage": "arthetaine/SternMokokoHint9.jpg"
+        },
+        {
+            "x": -261.09,
+            "y": 212.44,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 35,
+            "hintImage": "arthetaine/SternMokokoHint10.jpg"
+        },
+        {
+            "x": -258.41,
+            "y": 214.32,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 36,
+            "hintImage": "arthetaine/SternMokokoHint11.jpg"
+        },
+        {
+            "x": -258.28,
+            "y": 217,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 37,
+            "hintImage": "arthetaine/SternMokokoHint12.jpg"
+        },
+        {
+            "x": -262.72,
+            "y": 216.88,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 38,
+            "hintImage": "arthetaine/SternMokokoHint13.jpg"
+        },
+        {
+            "x": -322.62,
+            "y": 211.76,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 39,
+            "teleportTo": [
+                -304,
+                150
+            ]
+        },
+        {
+            "x": -304.12,
+            "y": 148.77,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 40,
+            "teleportTo": [
+                -325,
+                212
+            ]
+        },
+        {
+            "x": -141.12,
+            "y": 185.28,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 41,
+            "title": "Nest of Battle Ants Dungeon"
+        },
+        {
+            "x": -173.62,
+            "y": 151.53,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 42,
+            "title": "To Nebelhorn"
+        },
+        {
+            "x": -243.87,
+            "y": 202.02,
+            "type": MarkerTypeEnum.Zoning,
+            "id": 43,
+            "title": "To Stern",
+            "teleportTo": [
+                -284,
+                186
+            ]
+        },
+        {
+            "x": -211.59,
+            "y": 183.26,
+            "type": MarkerTypeEnum.FoodIngredient,
+            "id": 44,
+            "popupText": "진공 건조 가루 X-3",
+            "hintText": "아르데타인 요리사NPC - 만능 조리기<br>케나인 전투식량 C형 식재료",
+            "hintImage": "arthetaine/CrackedFoodIngredientHint1.jpg"
+        },
+        {
+            "x": -205.03,
+            "y": 196.63,
+            "type": MarkerTypeEnum.FoodIngredient,
+            "id": 45,
+            "popupText": "진공 건조 가루 X-1",
+            "hintText": "아르데타인 요리사NPC - 만능 조리기<br>케나인 전투식량 C형 식재료",
+            "hintImage": "arthetaine/CrackedFoodIngredientHint2.jpg"
+        },
+        {
+            "x": -195.41,
+            "y": 192.69,
+            "type": MarkerTypeEnum.Vista,
+            "id": 46,
+            "popupText": "계곡 및 로봇 잔해",
+            "hintImage": "arthetaine/CrackedVistaHint1.jpg"
+        },
+        {
+            "x": -210.34,
+            "y": 206.31,
+            "type": MarkerTypeEnum.SecretPassage,
+            "id": 47,
+            "hintText": "grants access to the 3 Mokoko seeds and the Treasure",
+            "hintImage": "arthetaine/CrackedSecretHint1.jpg"
+        },
+        {
+            "x": -202.59,
+            "y": 229,
+            "type": MarkerTypeEnum.Boss,
+            "id": 48,
+            "popupText": "DIS _MK-Ⅱ 프로토타입",
+            "hintImage": "arthetaine/CrackedBossHint1.jpg"
+        },
+        {
+            "x": -188.53,
+            "y": 214.19,
+            "type": MarkerTypeEnum.Boss,
+            "id": 49,
+            "popupText": "노련한 밀거래꾼 행동 대장",
+            "hintImage": "arthetaine/CrackedBossHint2.jpg"
+        },
+        {
+            "x": -199.09,
+            "y": 216.5,
+            "type": MarkerTypeEnum.TreasureBox,
+            "id": 50,
+            "popupText": "상자(낡은 금열쇠)"
+        },
+        {
+            "x": -166.91,
+            "y": 198.38,
+            "type": MarkerTypeEnum.WorldBoss,
+            "id": 51,
+            "popupText": "시그나투스"
+        },
+        {
+            "x": -167.34,
+            "y": 189.82,
+            "type": MarkerTypeEnum.HiddenStory,
+            "id": 52,
+            "popupText": "RGM 리포트 (Unforeseen Disaster)",
+            "hintImage": "arthetaine/CrackedHiddenStoryHint1.jpg"
+        },
+        {
+            "x": -222.22,
+            "y": 175.07,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 53,
+            "hintImage": "arthetaine/CrackedMokokoHint1.jpg"
+        },
+        {
+            "x": -196.03,
+            "y": 173.13,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 54,
+            "hintImage": "arthetaine/CrackedMokokoHint2.jpg"
+        },
+        {
+            "x": -182.22,
+            "y": 182.94,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 55,
+            "hintImage": "arthetaine/CrackedMokokoHint3.jpg"
+        },
+        {
+            "x": -178.59,
+            "y": 167.19,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 56,
+            "hintText": "Have to complete quest 'Unforeseen Disaster' (\"RGM 리포트\" near the world boss in this zone)",
+            "hintImage": "arthetaine/CrackedMokokoHint4.jpg"
+        },
+        {
+            "x": -198.72,
+            "y": 217.62,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 57,
+            "hintImage": "arthetaine/CrackedMokokoHint5.jpg",
+            "hintText": "3 seeds at this location"
+        },
+        {
+            "x": -200.09,
+            "y": 217.87,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 58,
+            "hintImage": "arthetaine/CrackedMokokoHint5.jpg",
+            "hintText": "3 seeds at this location"
+        },
+        {
+            "x": -200.22,
+            "y": 216.74,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 59,
+            "hintImage": "arthetaine/CrackedMokokoHint5.jpg",
+            "hintText": "3 seeds at this location"
+        },
+        {
+            "x": -202.91,
+            "y": 227.43,
+            "type": MarkerTypeEnum.Mokoko,
+            "id": 60,
+            "hintImage": "arthetaine/CrackedMokokoHint6.jpg"
         }
     ];
 
