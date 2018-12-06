44b04b85efc5dc49ae8751ec1fcc3d8057cc1678
Patrick Gustaevel <craieldev@gmail.com>
2018-12-05 23:42:21 -0500

    statistics revamp for per-area marker stats

--- src/statistics.js
+++ src/statistics.js
@@ -4,6 +4,7 @@
 
         constructor() {
             this.entries = {};
+            this.sections = {};
             this.nextEntryId = 0;
             this.suspendUpdate = true;
         }
@@ -17,59 +18,32 @@
 
         initializeUI() {
 
-            let cardPanel = $('#statisticsCardPanel');
-            cardPanel.empty();
+        }
 
-            this.addStatPanel({
-                id: 'cards',
-                icon: 'stat-cards.svg',
-                value: LAM.cards.count,
-                title: 'Cards'
-            });
+        addStatSection(id, title) {
+            let element = $('<div class="statisticsSection">' +
+                '<h2 class="text-center">' + title + '</h2><br>' +
+                '<div class="row statisticsContent" id="stat-section-' + id + '"></div></div><hr style="margin: 50px">');
 
-            for (let typeName in MarkerTypeEnum) {
-                let markerImage = MarkerTypeEnum[typeName];
-                switch (markerImage) {
-                    // Ignore some markers
-                    case MarkerTypeEnum.Internal:
-                    case MarkerTypeEnum.Notice:
-                    case MarkerTypeEnum.Zoning:
-                    case MarkerTypeEnum.ZoningIslandPVP:
-                    case MarkerTypeEnum.ZoningIsland:
-                    case MarkerTypeEnum.ZoningIslandFlux:
-                    case MarkerTypeEnum.ZoningWorld:
-                    case MarkerTypeEnum.TargetMark:
-                    case MarkerTypeEnum.SeaAreaAquatic:
-                    case MarkerTypeEnum.SeaAreaDeath:
-                    case MarkerTypeEnum.SeaAreaIce:
-                    case MarkerTypeEnum.SeaAreaSandstorm:
-                    case MarkerTypeEnum.SeaAreaSiren:
-                    case MarkerTypeEnum.SeaAreaStorm: {
-                        continue;
-                    }
-                }
-
-                this.addStatPanel({
-                    id: typeName,
-                    icon: markerImage,
-                    value: 0,
-                    title: MarkerTypeDefaultTitle(markerImage)
-                });
-            }
+            $('#content_statistics').append(element);
+            this.sections[id] = element;
         }
 
         addStatPanel(data) {
+
+
             if(data.id === undefined) {
                 data.id = this.nextEntryId++;
             }
 
             let a = '<div class="col-sm-4"><div class="card"><div class="card-content"><div class="card-body"><div class="media d-flex">';
             let icon = '<div class="align-self-center"><img class="statisticsIcon" src="images/icons/' + data.icon + '"/></div>';
-            let text = '<div class="media-body text-right"><h3 id="stat-value-' + data.id + '">' + data.value + '</h3><span>' + data.title + '</span></div>';
+            let text = '<div class="media-body text-right"><h3 id="stat-value-'  + data.section + '-' + data.id + '">' + data.value + '</h3><span>' + data.title + '</span></div>';
             let b = '</div></div></div></div></div>';
 
             this.entries[data.id] = data;
-            $('#statisticsCardPanel').append($(a + icon + text + b));
+            let element = $(a + icon + text + b);
+            $('#stat-section-' + data.section).append(element);
         }
 
         rebuildStats() {
@@ -77,31 +51,85 @@
                 return;
             }
 
+            $('#content_statistics').empty();
+            this.addStatSection('content', 'Content');
+
+            this.addStatPanel({
+                id: 'cards',
+                icon: 'stat-cards.svg',
+                value: LAM.cards.count,
+                title: 'Cards',
+                section: 'content'
+            });
+
             let markerStats = {};
+            let markerAreaStats = {};
             for (let name in LAM.areas) {
+                markerAreaStats[name] = {};
                 let area = LAM.areas[name];
                 for (let i in area.markerLayer.markers) {
                     let markerData = area.markerLayer.markers[i];
+
+                    switch (markerData.type) {
+                        // Ignore some markers
+                        case MarkerTypeEnum.Internal:
+                        case MarkerTypeEnum.Notice:
+                        case MarkerTypeEnum.Zoning:
+                        case MarkerTypeEnum.ZoningIslandPVP:
+                        case MarkerTypeEnum.ZoningIsland:
+                        case MarkerTypeEnum.ZoningIslandFlux:
+                        case MarkerTypeEnum.ZoningWorld:
+                        case MarkerTypeEnum.TargetMark:
+                        case MarkerTypeEnum.SeaAreaAquatic:
+                        case MarkerTypeEnum.SeaAreaDeath:
+                        case MarkerTypeEnum.SeaAreaIce:
+                        case MarkerTypeEnum.SeaAreaSandstorm:
+                        case MarkerTypeEnum.SeaAreaSiren:
+                        case MarkerTypeEnum.SeaAreaStorm: {
+                            continue;
+                        }
+                    }
+
                     if (markerStats[markerData.type] === undefined) {
                         markerStats[markerData.type] = 0;
                     }
 
+                    if(markerAreaStats[name][markerData.type] === undefined) {
+                        markerAreaStats[name][markerData.type] = 0;
+                    }
+
                     markerStats[markerData.type]++;
+                    markerAreaStats[name][markerData.type]++;
                 }
             }
 
-            for (let typeName in MarkerTypeEnum) {
-                if(this.entries[typeName] === undefined) {
-                    continue;
-                }
+            this.addStatSection('marker-all', 'Markers - All');
+            for(let name in markerAreaStats) {
+                this.addStatSection('marker-' + name.toLowerCase().replace(' ', '-'), 'Markers - ' + name);
+            }
 
-                let markerImage = MarkerTypeEnum[typeName];
-                let markerCount = markerStats[markerImage];
-                if(markerCount === undefined) {
-                    markerCount = 0;
-                }
+            let keys = Object.keys(markerStats).sort();
+            for (let i in keys) {
+                let type = keys[i];
+                this.addStatPanel({
+                    icon: type,
+                    value: markerStats[type],
+                    title: MarkerTypeDefaultTitle(type),
+                    section: 'marker-all'
+                });
+            }
 
-                $('#stat-value-' + typeName).text(markerCount);
+            for (let name in markerAreaStats) {
+                keys = Object.keys(markerAreaStats[name]).sort();
+                for (let i in keys) {
+                    let type = keys[i];
+                    this.addStatPanel({
+                        icon: type,
+                        value: markerAreaStats[name][type],
+                        title: MarkerTypeDefaultTitle(type),
+                        section: 'marker-' + name.toLowerCase().replace(' ', '-')
+                    });
+                }
             }
         }
     }
